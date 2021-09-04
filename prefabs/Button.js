07fe90877e1b67e0186d1cf76a075ea969511a8d
phucbm <hiphucbui@gmail.com>
2021-09-04 17:08:09 +0700

    🏗 Buttons: add disabled state, add atlas texture

--- prefabs/Button.js
+++ prefabs/Button.js
@@ -1,7 +1,8 @@
 class Button{
     constructor(ctx, x, y, obj){
+        // validate
         this.ctx = ctx;
-
+        this.atlas = obj.atlas || undefined;
         this.idleTexture = obj.idleTexture;
         this.activeTexture = obj.activeTexture;
         this.hasActiveState = typeof obj.activeTexture === 'string';
@@ -10,50 +11,62 @@ class Button{
         this.width = obj.width || 55;
         this.height = obj.height || 55;
         this.anchor = obj.anchor || {};
-        this.depth = obj.depth || undefined;
+        this.depth = obj.depth || 1;
+        this.disabled = obj.disabled || false;
 
         // create button using image object
-        this.button = this.ctx.add.image(x, y, this.idleTexture).setInteractive().setScale(this.originalScale);
+        if(typeof this.atlas !== 'undefined'){
+            this.button = this.ctx.add.image(x, y, this.atlas, this.idleTexture);
+        }else{
+            this.button = this.ctx.add.image(x, y, this.idleTexture)
+        }
+
+        this.button.setInteractive().setScale(this.originalScale).setDepth(this.depth);
 
         // update size
         this.button.setDisplaySize(this.width, this.height);
 
-        // pointer down
-        this.button.on('pointerdown', function(){
-            // callback
-            if(typeof obj.pointerDown === 'function'){
-                obj.pointerDown();
-            }
-
+        // Event: pointer down
+        this.button.on('pointerdown', () => {
             // click effect
             this.button.setDisplaySize(this.width * 0.9, this.height * 0.9);
 
             // click sound
             this.ctx.sys.game.CONFIG.sound.playSoundFx('click');
-        }, this);
 
-        // pointer up
+            // check disabled
+            if(this.disabled) return;
+
+            // callback
+            if(typeof obj.pointerDown === 'function'){
+                obj.pointerDown();
+            }
+        });
+
+        // Event: pointer up
         this.button.on('pointerup', function(){
+            // release effect
+            this.button.setDisplaySize(this.width, this.height);
+
+            // check disabled
+            if(this.disabled) return;
+
+            // change texture
+            this.updateStatus(!this.isActive);
+
             // callback
             if(typeof obj.pointerUp === 'function'){
                 obj.pointerUp();
             }
-
-            // release effect
-            this.button.setDisplaySize(this.width, this.height);
-
-            // change texture
-            this.updateStatus(!this.isActive);
         }, this);
 
         // update status
         this.updateStatus(this.isActive);
 
         // anchor
-        this.ctx.plugins.get('rexanchorplugin').add(this.button, this.anchor);
-
-        // depth
-        this.button.setDepth(this.depth);
+        if(typeof this.anchor !== 'undefined'){
+            this.ctx.plugins.get('rexanchorplugin').add(this.button, this.anchor);
+        }
     }
 
     get(){

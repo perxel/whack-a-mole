1eb1afb4b1ae0d77aaa486ffb321b78dff0c3a38
phucbm <hiphucbui@gmail.com>
2021-09-05 20:37:45 +0700

    🪄 character hurt state

--- prefabs/Character.js
+++ prefabs/Character.js
@@ -11,11 +11,13 @@ class Character{
         }
 
         this.scene = config.scene;
+        this.id = config.id || generateID();
         this.x = config.x || 0;
         this.y = config.y || 0;
         this.showHitArea = config.showHitArea || false;
         this.name = config.name || '';
         this.anchor = config.anchor || {};
+        this.hurtTime = 2000; // ms
 
         this.createAnimations();
         this.createPorcupine();
@@ -26,21 +28,27 @@ class Character{
     createAnimations(){
         // create frames
         const idleFrames = this.scene.anims.generateFrameNames('whack', {
-            start: 1, end: 4, zeroPad: 0,
+            start: 1, end: 4,
             prefix: `characters/${this.name}/smirky-`
         });
+        const attackFrame = this.scene.anims.generateFrameNames('whack', {
+            frames: [1],
+            prefix: `characters/${this.name}/hurt-`
+        });
         const hurtFrames = this.scene.anims.generateFrameNames('whack', {
-            start: 2, end: 3, zeroPad: 0,
+            start: 2, end: 3,
             prefix: `characters/${this.name}/hurt-`
         });
 
         // create anims
         this.scene.anims.create({key: `${this.name}-idle`, frames: idleFrames, frameRate: 8, repeat: -1, yoyo: true});
+        this.scene.anims.create({key: `${this.name}-attack`, frames: attackFrame, frameRate: 2, duration: 0.5});
         this.scene.anims.create({key: `${this.name}-hurt`, frames: hurtFrames, frameRate: 8, repeat: -1, yoyo: true});
     }
 
     createPorcupine(){
         this.character = this.scene.add.sprite(this.x, this.y, 'whack', `characters/${this.name}/normal-1`);
+        this.hurtTimer = undefined;
 
         // Set interactive
         this.character.setInteractive();
@@ -52,7 +60,7 @@ class Character{
         // Show hit area
         if(this.showHitArea){
             const hitArea = this.character.input.hitArea;
-            const hitAreaGraphic = this.scene.add.rectangle(this.character.x, this.character.y, hitArea.width, hitArea.height, 0x00ff00, 0.1);
+            const hitAreaGraphic = this.scene.add.rectangle(this.character.x, this.character.y, hitArea.width, hitArea.height, 0x00ff00, 0.15);
             hitAreaGraphic.setOrigin(this.character.originX, this.character.originY);
         }
 
@@ -63,11 +71,33 @@ class Character{
     }
 
     playAnimation(key){
+        // play animation
         this.character.anims.play(`${this.name}-${key}`);
+
+        // on attack
+        if(key === 'attack'){
+            // play hurt state
+            setTimeout(() => {
+                this.playAnimation('hurt');
+            }, 100);
+
+            // back to idle state
+            if(this.hurtTimer){
+                this.hurtTimer.remove();
+            }
+            this.hurtTimer = this.scene.time.addEvent({
+                delay: this.hurtTime,
+                callback: () => {
+                    this.playAnimation('idle');
+                }
+            });
+        }
+
+        // re-align
         this.scene.plugins.get('rexanchorplugin').add(this.character, this.anchor);
     }
 
     attack(){
-        this.playAnimation('hurt');
+        this.playAnimation('attack');
     }
 }

a7097937fdd5db918ca57ea034c343302190238e
phucbm <hiphucbui@gmail.com>
2021-09-19 13:48:08 +0700

    🏗 remake Button class

--- prefabs/Button.js
+++ prefabs/Button.js
@@ -1,86 +1,116 @@
 class Button{
-    constructor(ctx, x, y, obj){
+    constructor(config){
         // validate
-        this.ctx = ctx;
-        this.atlas = obj.atlas || undefined;
-        this.idleTexture = obj.idleTexture;
-        this.activeTexture = obj.activeTexture;
-        this.hasActiveState = typeof obj.activeTexture === 'string';
-        this.isActive = false;
-        this.originalScale = typeof obj.scale === 'number' ? obj.scale : 1;
-        this.width = obj.width || 55;
-        this.height = obj.height || 55;
-        this.anchor = obj.anchor || {};
-        this.depth = obj.depth || 1;
-        this.disabled = obj.disabled || false;
-
-        // create button using image object
-        if(typeof this.atlas !== 'undefined'){
-            this.button = this.ctx.add.image(x, y, this.atlas, this.idleTexture);
-        }else{
-            this.button = this.ctx.add.image(x, y, this.idleTexture)
+        if(!config.scene){
+            console.warn("Missing scene!");
+            return;
         }
 
-        this.button.setInteractive().setScale(this.originalScale).setDepth(this.depth);
+        this.scene = config.scene;
+        this.sceneKey = this.scene.scene.key;
 
-        // update size
-        this.button.setDisplaySize(this.width, this.height);
+        // assign event to buttons
+        this.assign();
+    }
 
-        // Event: pointer down
-        this.button.on('pointerdown', () => {
-            // click effect
-            this.button.setDisplaySize(this.width * 0.9, this.height * 0.9);
+    assign(callback){
+        const $buttons = $('.button');
+        $buttons.each((i, el) => {
+            const $button = $(el);
+            const type = $button.attr("data-button").value;
+            const isDisabled = $button.hasClass('disabled');
 
-            // click sound
-            this.ctx.sys.game._SOUND.playSoundFx('click');
-
-            // check disabled
-            if(this.disabled) return;
-
-            // callback
-            if(typeof obj.pointerDown === 'function'){
-                obj.pointerDown();
+            // check flag
+            if($button.hasClass("event-added")){
+                return;
             }
+
+            // set flag
+            $button.addClass("event-added");
+
+            $button.on('click', () => {
+                // click sound
+                this.scene.sys.game._SOUND.playSoundFx('click');
+
+
+                // skip disabled
+                if(isDisabled) return;
+
+                // check type
+                switch(type){
+                    case 'sound-toggle':
+                        // update on load
+                        if(this.scene.sys.game._SOUND.isPlaying()){
+                            $button.addClass("active");
+                        }
+
+                        // toggle sound on click
+                        if(this.scene.sys.game._SOUND.isPlaying()){
+                            this.scene.sys.game._SOUND.pause();
+                            $button.removeClass("active");
+                        }else{
+                            this.scene.sys.game._SOUND.play();
+                            $button.addClass("active");
+                        }
+                        break;
+                    case 'back':
+                        this.goBack();
+                        break;
+                    case 'how-to-play':
+                        this.scene.scene.start('HowToPlay', {previousScene: this.scene.sceneData});
+                        break;
+                    case 'choose-level':
+                        this.scene.scene.start('ChooseLevel', {previousScene: this.scene.sceneData});
+                        break;
+                    case 'menu':
+                        this.scene.scene.start('Menu', {previousScene: this.scene.sceneData});
+                        break;
+                    case 'full-screen-toggle':
+                        // todo: DOM disappear in full screen
+                        this.scene.scale.toggleFullscreen();
+                        break;
+                    case 'go-level':
+                        const level = $button.attr("data-level").value;
+                        if(DEV) console.log(`Go to level ${level}`);
+                        this.scene.scene.start("GamePlay", {previousScene: this.scene.sceneData, levelID: level});
+                        break;
+                }
+            });
         });
 
-        // Event: pointer up
-        this.button.on('pointerup', function(){
-            // release effect
-            this.button.setDisplaySize(this.width, this.height);
+        this.updateButtonState();
 
-            // check disabled
-            if(this.disabled) return;
-
-            // change texture
-            this.updateStatus(!this.isActive);
-
-            // callback
-            if(typeof obj.pointerUp === 'function'){
-                obj.pointerUp();
-            }
-        }, this);
-
-        // update status
-        this.updateStatus(this.isActive);
-
-        // anchor
-        if(typeof this.anchor !== 'undefined'){
-            this.ctx.plugins.get('rexanchorplugin').add(this.button, this.anchor);
+        if(typeof callback === 'function'){
+            callback();
         }
     }
 
-    get(){
-        return this.button;
+    /**
+     * Go back
+     */
+    goBack(){
+        this.previousScene = this.scene.sceneData.previousScene || undefined;
+
+        if(!this.previousScene.name){
+            this.previousScene.name = "Menu";
+            if(DEV) console.log(`Undefined previous scene. Go back from ${this.sceneKey} to ${this.previousScene.name}.`);
+        }else{
+            if(DEV) console.log(`Go back from ${this.sceneKey} to ${this.previousScene.name}`);
+        }
+
+        this.scene.scene.start(this.previousScene.name, {previousScene: this.scene.sceneData});
     }
 
-    updateStatus(active){
-        if(this.hasActiveState){
-            this.isActive = active;
-            if(this.isActive){
-                this.button.setTexture(this.activeTexture);
-            }else{
-                this.button.setTexture(this.idleTexture);
-            }
+
+    /**
+     * Update button state
+     */
+    updateButtonState(){
+        const $button = $('[data-button="sound-toggle"]');
+        if(this.scene.sys.game._SOUND.isPlaying()){
+            $button.addClass("active");
+        }else{
+            $button.removeClass("active");
         }
     }
 }
